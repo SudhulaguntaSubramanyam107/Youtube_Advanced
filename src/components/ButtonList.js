3a51b7ceb7055c9dff59dcc54e2ecb858a33ee2a
sudhulaguntasubramanyam107 <sunnycreative19@gmail.com>
2026-03-12 10:14:00 +0530

    deploy error fixed

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,6 +1,6 @@
 import Button from "./Button"
 const Buttonlist = ()=>{
-    const buttonListName = ["All","Music","Mixes", "Telugu cinema", "Movie Musicas", "AI", "One day internations", "Computer programming","Comedy","Jai hanuman"]
+    const buttonListName = ["All","Music","Mixes", "Telugu cinema", "Movie Musicas", "AI", "One day internations", "Computer programming"]
     return (
         <div className="flex gap-5 whitespace-nowrap">
             {buttonListName.map(item=>(<Button key = {item} name = {item}/>))}
